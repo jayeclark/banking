c35ad0f993659bcb84d357d001894bb2fc6f8be1
Jay Clark <jayelizabethclark@gmail.com>
2021-10-01 18:26:13 -0400

    2.3.2 Nav styling

--- src/components/AppNav.js
+++ src/components/AppNav.js
@@ -5,7 +5,6 @@ function AppNav() {
     
     const changeActive = e => {
        let targetEl = e.currentTarget;
-       console.log(targetEl);
        let link = targetEl.getElementsByClassName('nav-link')[0];
        let currentlyActive = Array.from(document.getElementsByClassName('active'));
         currentlyActive.forEach(el => el.classList.remove('active'));
@@ -13,26 +12,26 @@ function AppNav() {
     }  
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor:"#c41230", fontWeight: "500"}}>
             <div className="container-fluid">
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent2" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent2">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li id="home-link" onClick={e=> changeActive(e)} className="nav-item" data-for="home-link-tooltip"  data-iscapture="true" data-tip="Return to Home">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0" style={{boxSizing:"border-box",width:"100%",justifyContent:"center"}}>
+                        <li id="home-link" style={{padding:"0px 20px"}} onClick={e=> changeActive(e)} className="nav-item" data-for="home-link-tooltip"  data-iscapture="true" data-tip="Return to Home">
                             <Link to="/" className="nav-link active">Home</Link>
                         </li>
-                        <li id="create-account-link" onClick={e=> changeActive(e)} className="nav-item" data-for="create-account-link-tooltip"  data-iscapture="true" data-tip="Create an Account">
+                        <li id="create-account-link" style={{padding:"0px 20px"}} onClick={e=> changeActive(e)} className="nav-item" data-for="create-account-link-tooltip"  data-iscapture="true" data-tip="Create an Account">
                             <Link to="/create-account/" className="nav-link">Create Account</Link>
                         </li>
-                        <li id="deposit-link" onClick={e=> changeActive(e)} className="nav-item" data-for="deposit-link-tooltip"  data-iscapture="true" data-tip="Deposit to Your Account">
+                        <li id="deposit-link" style={{padding:"0px 20px"}} onClick={e=> changeActive(e)} className="nav-item" data-for="deposit-link-tooltip"  data-iscapture="true" data-tip="Deposit to Your Account">
                             <Link to="/deposit/" className="nav-link">Deposit</Link>
                         </li>
-                        <li id="withdraw-link" onClick={e=> changeActive(e)} className="nav-item" data-for="withdraw-link-tooltip"  data-iscapture="true" data-tip="Withdraw from Your Account">
+                        <li id="withdraw-link" style={{padding:"0px 20px"}} onClick={e=> changeActive(e)} className="nav-item" data-for="withdraw-link-tooltip"  data-iscapture="true" data-tip="Withdraw from Your Account">
                             <Link to="/withdraw/" className="nav-link">Withdraw</Link>
                         </li>
-                        <li id="all-data-link" onClick={e=> changeActive(e)} className="nav-item" data-for="all-data-link-tooltip"  data-iscapture="true" data-tip="View All Transaction Data">
+                        <li id="all-data-link" style={{padding:"0px 20px"}} onClick={e=> changeActive(e)} className="nav-item" data-for="all-data-link-tooltip"  data-iscapture="true" data-tip="View All Transaction Data">
                             <Link to="/all-data/" className="nav-link">All Data</Link>
                         </li>
                     </ul>
