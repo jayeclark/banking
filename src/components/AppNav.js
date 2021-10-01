12cad429a60588e843663a9f110ce363965b5bc2
Jay Clark <jayelizabethclark@gmail.com>
2021-10-01 19:25:09 -0400

    Tweak navbar settings

--- src/components/AppNav.js
+++ src/components/AppNav.js
@@ -23,13 +23,13 @@ function AppNav() {
                     }
 
     return (
-        <nav className="navbar navbar-expand-md navbar-dark" style={{backgroundColor:"#c41230", fontWeight: "500"}}>
-            <div className="container-fluid">
+        <nav className="navbar navbar-expand-sm navbar-dark" style={{backgroundColor:"#c41230", fontWeight: "500"}}>
+            <div className="container-fluid" style={{justifyContent:"left"}}>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent2" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent2">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0" style={{boxSizing:"border-box",width:"100%",justifyContent:"center"}}>
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0" style={{boxSizing:"border-box",marginTop:'4px',width:"100%"}}>
                         <li id="home-link" style={{padding:"0px 20px"}} onClick={e=> changeActive(e)} className="nav-item" data-for="home-link-tooltip"  data-iscapture="true" data-tip="Return to Home">
                             <Link to="/" className={"nav-link" + active.home}>Home</Link>
                         </li>
