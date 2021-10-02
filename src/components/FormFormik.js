b9bc8bf28923cdd510a074d70ca773d8168502aa
Jay Clark <jayelizabethclark@gmail.com>
2021-10-02 14:40:43 -0400

    Add account form

--- src/components/FormFormik.js
+++ src/components/FormFormik.js
@@ -1,10 +1,82 @@
-import Formik from 'formik';
+import { useFormik } from 'formik';
 
-function FormFormik({formFields, formSubmission}) {
+function FormFormik({formFields, formSubmission, accountCreated, setAccountCreated}) {
 
+    let {buttons, success, idRoot} = formSubmission
+    let initialFieldValues = {};
+
+    for (let field in formFields) {
+        const fieldName = formFields[field].name;
+        initialFieldValues[fieldName] = '';
+    }
+
+    const formik = useFormik({
+
+        initialValues: initialFieldValues,
+
+        onSubmit: values => {
+            console.log(values);
+            alert(success);
+            for (let field in formFields) {
+                let element = document.getElementById(formFields[field].name);
+                element.value = '';
+                values[formFields[field].name] = '';
+            }
+            accountCreated = true;
+            const buttonArr = formSubmission.buttons.filter(x=> x.type="submit")[0];
+            console.log(buttonArr);
+            let buttonEl = document.getElementById(idRoot+"-"+buttonArr.name);
+            console.log(buttonEl);
+            buttonEl.innerHTML = buttonArr.altDisplay;
+        },
+
+        validate: values => {
+            let errors = {};
+            for (let field in formFields) {
+                const thisField = formFields[field];
+                const validation = thisField.validation;
+
+                for (let item of validation) {
+                    let validationFunction = item.function;
+                    let errorMsg = item.error;
+                    let value = values[thisField.name];
+                    if(validationFunction(value) === false) { errors[thisField.name] = errorMsg }
+                }
+
+            }
+
+            return errors;
+
+        }
+
+    })
 
     return (
-        <div>Formik Form Goes Here</div>
+        <form onSubmit={formik.handleSubmit}>
+            {formFields.map((field,i)=>{
+                return (
+                    <div key={i}>
+                        <div>{field.display}</div>
+                        <div>
+                            <input type={field.type} autoComplete="off" id={field.name} name={field.name} onChange={formik.handleChange} value={formik.values[field.name]}/>
+                            {formik.errors[field.name] ? <div id={idRoot + "-" + field.name + "Error"}>{formik.errors[field.name]}</div> : null }
+                        </div>
+                    </div>
+                )
+            })}
+            <div>
+                {buttons.map((buttonEl,i)=>{
+                    return (
+                        <div key={i}>
+                            <button id={idRoot + "-" + buttonEl.name} className={Object.values(formik.errors).every(x=>'') ? buttonEl.className : buttonEl.className + ' disabled'} type={buttonEl.type}>{buttonEl.dependency() ? buttonEl.altDisplay : buttonEl.display}</button>
+                        </div>
+                    )
+                    
+                })}
+                
+            
+            </div>
+        </form>
     )
 
 }
--- src/pages/CreateAccount.js
+++ src/pages/CreateAccount.js
@@ -3,38 +3,48 @@ import FormReactFinal from '../components/FormReactFinal';
 import FormReactHook from '../components/FormReactHook';
 import Card from '../components/Card';
 
-
 function CreateAccount() {
+
+    const accountCreated = false;
 
     const formProvider = 'formik';
 
     const valLength = val => val.length > 0;
-    const regex = new RegExp('^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$');
-    const emailFormat = email => regex.test(email);
-    const passwordLength = val => val.length > 0;
+    const nameLength = val => val.length >= 4;
+    const emlRegex = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,'i');
+    const pwdRegex = new RegExp(/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{0,}$/,"i");
+    const emailFormat = email => emlRegex.test(email);
+    const passwordLength = val => val.length >= 8;
+    const passwordFormat = pwd => pwdRegex.test(pwd);
 
     const formFields = 
         [{  name: 'name',
             display: 'Name',
-            validation: [{function: valLength, error: 'Please enter your name'} ],
-            idRoot: 'create-account',
+            type: 'text',
+            validation: [{function: valLength, error: 'Please enter your name'},
+                         {function: nameLength, error: 'Name must be 4 or more characters long'}  ],
             },
             {
             name: 'email',
+            type: 'text',
             display: 'Email',
             validation: [{function: valLength, error: 'Please enter your email address'},
                         {function: emailFormat, error: 'Please enter a valid email address'}]
             },
             {
-                name: 'password',
-                display: 'Password',
-                validation: [{function: passwordLength, error: 'Password must be 8 characters or longer'}]
-                }
+            name: 'password',
+            type: 'password',
+            display: 'Password',
+            validation: [   {function: passwordLength, error: 'Must be 8 characters or longer'},
+                            {function: passwordFormat, error: 'Must include one letter, one number & one of these characters: @$!%*#?&'}
+                        ]
+            }
         ]
 
-    const formSubmission = {buttons:[{type:'submit',name:'submit',display:'Submit Account'}],
+    const formSubmission = {buttons:[{type:'submit',name:'submit',display:'Create Account', altDisplay: 'Add Another Account', dependency: () => accountCreated === true, className:'btn btn-primary'}],
                             success: 'Your account has been created!',
                             failure: 'There was a problem creating your account. Please contact the help desk for support.',
+                            idRoot: 'create-account',
                             }
 
 
@@ -46,19 +56,35 @@ function CreateAccount() {
     switch (formProvider) {
 
         case 'formik':
-            form = <FormFormik formFields={formFields} formSubmission={formSubmission}></FormFormik> ;
+            form = <FormFormik 
+                        formFields={formFields} 
+                        formSubmission={formSubmission} 
+                        accountCreated={accountCreated} >
+                    </FormFormik> ;
             break;
 
         case 'reactFinal':
-            form = <FormReactFinal formFields={formFields} formSubmission={formSubmission}></FormReactFinal>;
+            form = <FormReactFinal 
+                        formFields={formFields} 
+                        formSubmission={formSubmission} 
+                        accountCreated={accountCreated} >
+                    </FormReactFinal>;
             break;
 
         case 'reactHook':
-            form = <FormReactHook formFields={formFields} formSubmission={formSubmission}></FormReactHook>;
+            form = <FormReactHook 
+                        formFields={formFields} 
+                        formSubmission={formSubmission} 
+                        accountCreated={accountCreated} >
+                    </FormReactHook>;
             break;
         
         default:
-            form = <FormFormik formFields={formFields} formSubmission={formSubmission}></FormFormik>;
+            form = <FormFormik 
+                        formFields={formFields} 
+                        formSubmission={formSubmission} 
+                        accountCreated={accountCreated} >
+                    </FormFormik>;
     }
 
     return (
