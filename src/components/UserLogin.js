2c8b14825b7d70b51e99773e84b6e8b77b3be19c
Jay Clark <jayelizabethclark@gmail.com>
2021-10-04 17:49:55 -0400

    Add language toggler box

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -1,8 +1,10 @@
 import UserContext from "../helpers/UserContext";
 import UserDBContext from "../helpers/UserDBContext";
+import LanguageContext from '../helpers/LanguageContext';
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/userLogin.css';
+import languages from '../data/languages';
 
 function UserLogin() {
 
@@ -10,6 +12,10 @@ function UserLogin() {
 
     const {loggedInUser, logIn, logOut} = useContext(UserContext);
 
+    // Get language context and set language data
+    const {language, changeLanguage} = useContext(LanguageContext);
+    const data = languages[language];
+    const {signOut, signIn} = data.general;
 
     const getUser = (userDBContext, loggedInUser) => {
         return userDBContext.users.filter(x=>x.number === loggedInUser)[0];
@@ -39,9 +45,21 @@ function UserLogin() {
          link.classList.add('active');
      }  
 
+    const handleChange = e => {
+        const selectBox = document.getElementById("language-toggler");
+        const selectedValue = selectBox.options[selectBox.selectedIndex].value;
+        if (language !== selectedValue) {changeLanguage(selectedValue)}
+    }
+
     return (
-        <div style={{padding:'10px',fontSize:"0.8rem", height: 'auto', margin:'auto 0px', display:'flex',flexWrap:'nowrap'}}>
-        {(loggedInUser !== '') ? <div style={{padding: '0px 10px',borderRight: '1px solid #ccc'}}>{getUser(userDBContext,loggedInUser).name}</div> : null}<div className='login-link' onClick={loggedInUser !== '' ? handleSignOut : userDBContext.users.length > 0 ? handleSignIn : changeActive}>{loggedInUser !== ''  ? 'Sign Out' : userDBContext.users.length > 0 ? 'Sign In' : <Link style={{textDecoration:'none',color:'black'}} to="/create-account/">Sign In</Link>}</div>
+        <div style={{padding:'10px',fontSize:"0.8rem", height: 'auto', margin:'auto 0px', display:'flex',flexWrap:'nowrap',alignItems:'center'}}>
+        {(loggedInUser !== '') ? <div style={{padding: '2px 10px',borderRight: '1px solid #ccc'}}>{getUser(userDBContext,loggedInUser).name}</div> : null}<div className='login-link' onClick={loggedInUser !== '' ? handleSignOut : userDBContext.users.length > 0 ? handleSignIn : changeActive}>{loggedInUser !== '' ? signOut : userDBContext.users.length > 0 ? signIn : <Link style={{textDecoration:'none',color:'black'}} to="/create-account/">{signIn}</Link>}</div>
+            <div class="language-toggle-container">
+                <select id="language-toggler"  class="language-toggle" defaultValue={language} onChange={handleChange} tabIndex="0">
+                    <option value="en">English</option>
+                    <option value="es">Spanish</option>
+                </select>
+            </div>
         </div>
     )
 }
