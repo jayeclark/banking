8783fce9997803bc294814313a61e734ae069587
Jay Clark <jayelizabethclark@gmail.com>
2021-10-06 15:27:31 -0400

    Fix validation function naming convention

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -1,15 +1,23 @@
-const hasInput = (val, len=0) => val.length > len;
-const isPositive = (val) => val.toString().search(/^-/) === -1;
-const isNumber = (val) => val.toString().search(/^(\d{1,3}){0,1}(,\d\d\d){0,}((\.)((\d){0,2})){0,1}$/) > -1;
-const hasTwoSigFigs = (val) => val.toString().search(/\.(\d){3,}/) === -1;
-const isLessThanBalance = (val, balance) => Number(val.replaceAll(',','')) <= balance ||  val.toString().search(/^(\d{1,3}){0,1}(,\d\d\d){0,}((\.)((\d){0,2})){0,1}$/)  === -1;
-const emailFormat = email => {
-    const emlRegex = new RegExp(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,'i');
-    return emlRegex.test(email);}
-const passwordFormat = pwd => {
-    const pwdRegex = new RegExp(/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{0,}$/,"i");
-    return pwdRegex.test(pwd);}
+const hasInput = (val, len=1) => val.length >= len;
 
-const validationFunctions = {hasInput, isPositive, isNumber, hasTwoSigFigs, isLessThanBalance, emailFormat, passwordFormat };
+const isPositive = val => val.toString().search(/^-/) === -1;
+
+const isNumber = val => val.toString().search(/^(\d{1,3}){0,1}(,\d\d\d){0,}((\.)((\d){0,2})){0,1}$/) > -1;
+
+const hasTwoSigFigs = val => val.toString().search(/\.(\d){3,}/) === -1;
+
+const isLessThanBalance = (val, balance) => Number(val.replaceAll(',','')) <= balance ||  val.toString().search(/^(\d{1,3}){0,1}(,\d\d\d){0,}((\.)((\d){0,2})){0,1}$/)  === -1;
+
+const isValidEmailFormat = email => {
+        const emlRegex = new RegExp(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,'i');
+        return emlRegex.test(email);
+    }
+
+const isValidPasswordFormat = pwd => {
+        const pwdRegex = new RegExp(/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{0,}$/,"i");
+        return pwdRegex.test(pwd);
+    }
+
+const validationFunctions = { hasInput, isPositive, isNumber, hasTwoSigFigs, isLessThanBalance, isValidEmailFormat, isValidPasswordFormat };
 
 export default validationFunctions
