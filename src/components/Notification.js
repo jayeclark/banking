4450321c2600fc1de09ef74417e456ec5fb3b4cc
Jay Clark <jay@jayeclark.dev>
2021-10-24 11:48:37 -0400

    Improve toast notifications
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,15 +1,28 @@
 import '../styles/Notification.css';
+import { useState } from 'react';
 import Card from './Card.js';
 import closeButton from '../assets/closeButton.svg';
+import { useEffect } from 'react/cjs/react.development';
 
-function Notification({title, text, type, handleClick}) {
+function Notification({title, text, type, handleClick, time}) {
 
     let parsedContent = title ? <><div className="close-button"><div className="close-x" onClick={(e) => handleClick()}><img src={closeButton} alt="" height="20px" style={{margin:"-10px 0px"}}/></div></div><div style={{padding:"30px"}}><h4>{title}</h4>{text}</div></>
                                 : <><div className="close-button"><div className="close-x" onClick={(e) => handleClick()}><img src={closeButton} alt="" height="20px" style={{margin:"-10px 0px"}}/></div></div><div style={{padding:"30px"}}>{text}</div></>;
 
+    const seconds = time / 1000;
+
+    const [width, setWidth] = useState('100%');
+
+    useEffect(()=> setWidth('0%'),[])
+
     return (
         <div className="notification-container" >
-            <Card className={type === 'success' ? "notification-card green" : type === 'error' ? "notification-card red" : "notification-card"} content={parsedContent} style={{padding: '50px', fontWeight:'500'}}></Card>
+            <div className={type === 'success' ? "notification-card green" : type === 'error' ? "notification-card red" : "notification-card"}>
+                <Card content={parsedContent} style={{padding: '50px', fontWeight:'500'}}></Card> 
+                <div className={type === 'success' ? "notification-countdown-green" : type === 'error' ? "notification-countdown-red" : "notification-countdown"}>
+                    <div className={type === 'success' ? "countdown-progress-green" : type === 'error' ? "countdown-progress-red" : "countdown-progress"} style={{ width, transitionDuration: seconds + 's' }}></div>
+                </div>
+            </div> 
         </div>
     )
 }
