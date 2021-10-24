f41f42bece849d6e77ad39a3ff6a729ed9c9aeac
Jay Clark <jay@jayeclark.dev>
2021-10-24 11:48:47 -0400

    Add form library tooltip
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

--- src/components/OptionsNav.js
+++ src/components/OptionsNav.js
@@ -1,6 +1,7 @@
 import '../styles/OptionsNav.css';
 import { useContext } from 'react';
 import FormContext from '../helpers/FormContext';
+import ReactTooltip from 'react-tooltip';
 
 function OptionsNav() {
 
@@ -21,11 +22,12 @@ function OptionsNav() {
         <div className="option-nav-element hideable" style={{flexShrink:1, flexGrow: 1, width: '100%', border:'1px solid transparent'}}></div>
         <div className="option-nav-element form-provider-toggle right-justify">
           <div style={{whiteSpace: "nowrap", fontSize: "0.8rem", padding: '5px', fontWeight: 'bold'}}>Form library: </div>
-            <select id="form-select" className="form-select" onChange={handleChange}>
+            <select id="form-select" className="form-select" onChange={handleChange}  data-for="form-toggle-tip"  data-iscapture="true" data-tip="Select form library. (Resets any in-progress form.)">
               <option value="formik">Formik</option>
               <option value="reactHook">React Hook Form</option>
               <option value="reactFinal">React Final Form</option>
             </select>
+            <ReactTooltip id="form-toggle-tip" key="1" place="bottom" type="dark" effect="solid" multiline={true} />
         </div>
       </div>
     )
