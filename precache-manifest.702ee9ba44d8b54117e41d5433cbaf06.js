bc42ba6f74a3632b9548bf3a1d4fca1784fb2d71
Jay Clark <jay@jayeclark.dev>
2021-10-28 11:40:05 -0400

    Updates

---
+++ precache-manifest.702ee9ba44d8b54117e41d5433cbaf06.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34dd731ea30355b116eef31c1b292898",
+    "url": "/banking/index.html"
+  },
+  {
+    "revision": "d10bfc484d5f3fd5d2df",
+    "url": "/banking/static/css/main.c59e9d48.chunk.css"
+  },
+  {
+    "revision": "3004879386928c58180a",
+    "url": "/banking/static/js/2.3b6c89be.chunk.js"
+  },
+  {
+    "revision": "acaccd3a11c5c4db30b41f14d8e96ab6",
+    "url": "/banking/static/js/2.3b6c89be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cd6bc19e0ce3778a380",
+    "url": "/banking/static/js/3.019d6782.chunk.js"
+  },
+  {
+    "revision": "d10bfc484d5f3fd5d2df",
+    "url": "/banking/static/js/main.4cb9e470.chunk.js"
+  },
+  {
+    "revision": "0aa698ade14471e2070c",
+    "url": "/banking/static/js/runtime-main.d0dac3a0.js"
+  },
+  {
+    "revision": "2abe73a55a45254e9fd4095a734fa366",
+    "url": "/banking/static/media/closeButton.2abe73a5.svg"
+  },
+  {
+    "revision": "08b5310860b00ad8b14d52ed3d3f084d",
+    "url": "/banking/static/media/globe.08b53108.svg"
+  },
+  {
+    "revision": "39631568b3eed79d6b3bb8f01fe79cf4",
+    "url": "/banking/static/media/heroimage.39631568.jpeg"
+  },
+  {
+    "revision": "8501eddc227a8cd862db80fab40d73a6",
+    "url": "/banking/static/media/logo.8501eddc.svg"
+  }
+]);
