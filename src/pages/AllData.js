c31a085e5577226adb954e976f8442d82664b14e
Jay Clark <jay@jayeclark.dev>
2021-10-28 17:53:17 -0400

    Add font-awesome
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

--- src/pages/AllData.js
+++ src/pages/AllData.js
@@ -4,6 +4,8 @@ import Card from '../components/Card';
 import UserDBContext from '../helpers/UserDBContext';
 import LanguageContext from '../helpers/LanguageContext';
 import languages from '../data/languages.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import '../styles/AllData.css';
 
 function AllData() {
@@ -49,7 +51,15 @@ function UserChartRow({data}) {
     const maskPwd = (pwd) => pwd.substring(0,1) + '*'.repeat(pwd.length - 1);
 
     return (
-        <><div className="align-left">{ txnDate.toLocaleDateString() }</div><div className="align-left">{ data.name }</div><div className="align-left">{ data.email }</div><div style={{position:"relative"}} className="align-left"><span style={{display:"inline-block",padding:"0px 3px 0px 0px"}}>{ showPwd ? data.password : maskPwd(data.password) }</span><span onClick={ togglePwd } style={{ display: "inline-block", padding:"2px", position: "absolute", right: "0px", minWidth: "39px", textAlign:"center", margin:"2px", border:"1px solid #ddd", color:"#666", borderRadius:"5px", fontSize:"0.7em" }}>{ showPwd ? "Hide" : "Show" }</span></div></>
+        <>
+            <div className="align-left">{ txnDate.toLocaleDateString() }</div>
+            <div className="align-left">{ data.name }</div>
+            <div className="align-left">{ data.email }</div>
+            <div style={{position:"relative"}} className="align-left">
+                <span style={{display:"inline-block",padding:"0px 3px 0px 0px"}}>{ showPwd ? data.password : maskPwd(data.password) }</span>
+                <span onClick={ togglePwd } style={{ display: "inline-block", padding:"2px 8px", position: "absolute", right: "0px", textAlign:"center", margin:"2px", border:"1px solid #ddd", color:"#666", borderRadius:"7px", fontSize:"0.7em" }}>{ showPwd ? <FontAwesomeIcon icon={faEyeSlash} /> : <FontAwesomeIcon icon={faEye} /> }</span>
+            </div>
+        </>
     )
 }
 
